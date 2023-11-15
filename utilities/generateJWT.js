47706dd372ae9a986394b712d93919edcd404b55
silviosalza <silviofilipposalza@gmail.com>
2023-11-15 16:56:00 +0100

    added payload

--- utilities/generateJWT.js
+++ utilities/generateJWT.js
@@ -1,7 +1,12 @@
 const jwt = require("jsonwebtoken")
 
 module.exports = function(user){
-
-    return jwt.sign(user , process.env.JWT_SECRET , {expiresIn: "1h"})
+//estraggo i dati che voglio salvare nei token, tranne password
+    const payload = {
+        id: user.id,
+        username: user.username,
+    }
+//alla funzione sign passo payload, la JWT ed un metodo di expire
+    return jwt.sign(payload , process.env.JWT_SECRET , {expiresIn: "1h"})
 
 }
